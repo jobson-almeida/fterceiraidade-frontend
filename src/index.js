dc960a3f06653defa1f34e7d599421ac71ad9f45
Y%y#!Gyp^!E4KiUijYX7Q@xpLQV%&uR^$&YqE#YR9MhEq5z29Tx!fWYxXvoxent <jobson.ra@outlook.com>
2023-08-24 14:21:42 -0300

    build: project update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render((

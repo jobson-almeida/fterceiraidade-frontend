93f3374e2149dee25c56f4c22acb9cd0bd542b3c
Jobson Almeida <jobson.ra@outlook.com>
2023-12-30 16:09:06 -0300

    perf: create pre render

--- src/app/(pages)/courses/[id]/page.js
+++ src/app/(pages)/courses/[id]/page.js
@@ -1,37 +1,6 @@
-import Image from "next/image"
-
-async function getCourse(id) {
-  const response = await fetch(`http://localhost:3000/api/courses/${id}`, {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json"
-    },
-    cache: "no-cache"
-  })
-  return response.json()
-}
+import Course, { preload } from "@/app/components/course"
 
 export default async function Page({ params }) {
-  const course = await getCourse(params.id)
-
-  return (
-    <div className="flex flex-col gap-2 w-full max-w-[1024px]">
-      <div>{course.name}</div>
-      <div>{course.description}</div>
-      <div>
-        <Image
-          alt={course.name}
-          src={course.image}
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{
-            width: "100%",
-            height: "auto"
-          }}
-          priority
-        />
-      </div>
-    </div>
-  )
+  preload(params.id)
+  return <Course id={params.id} />
 }
---
+++ src/app/components/course.js
@@ -0,0 +1,45 @@
+import Image from "next/image"
+
+async function getCourse(id) {
+  const response = await fetch(`http://localhost:3000/api/courses/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json"
+    },
+    cache: "no-cache"
+  })
+  return response.json()
+}
+
+export const preload = (id) => {
+  void getCourse(id)
+}
+
+export default async function Course({ id }) {
+  const result = await getCourse(id)
+
+  if (result === null || result === undefined) {
+    throw new Error('Fails')
+  }
+
+  return (
+    <div className="flex flex-col gap-2 w-full max-w-[1024px]">
+      <div>{result.name}</div>
+      <div>{result.description}</div>
+      <div>
+        <Image
+          alt={result.name}
+          src={result.image}
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{
+            width: "100%",
+            height: "auto"
+          }}
+          priority
+        />
+      </div>
+    </div>
+  )
+}

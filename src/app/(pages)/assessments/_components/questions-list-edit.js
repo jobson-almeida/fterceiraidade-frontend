04c4d2a8f49c2a7d8b7a1967a1a7161d48e46514
Jobson Almeida <jobson.ra@outlook.com>
2023-12-29 21:26:03 -0300

    fix: get values for question list

--- src/app/(pages)/assessments/_components/questions-list-edit.js
+++ src/app/(pages)/assessments/_components/questions-list-edit.js
@@ -1,9 +1,23 @@
 "use client"
 import QuestionCard from "../../questions/_components/question-card"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { addFrom, removeFrom } from "../lib/functions-for-filters";
 
 export default function QuestionsListEdit({ data, ids, clear, valiant }) {
     const [questions, setQuestions] = useState([])
+
+    if (clear === true) {
+        const ISSERVER = typeof window === "undefined";
+        if (!ISSERVER && localStorage.getItem("questions_assessments") !== null) {
+            console.log(localStorage.getItem("questions_assessments"))
+            localStorage.removeItem("questions_assessments");
+        }
+    }
+
+    useEffect(() => {
+        if (clear) setQuestions(addFrom(data, ids))
+        if (!clear) setQuestions(removeFrom(data, ids))
+    }, [ids, data, clear])
 
     return (
         <>

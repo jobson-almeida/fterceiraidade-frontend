1ecca06943d7071d52f9b8b8d93969412aaf0f23
Jobson Almeida <jobson.ra@outlook.com>
2024-02-28 14:33:48 -0300

    build: test

--- src/app/(pages)/assessments/page.js
+++ src/app/(pages)/assessments/page.js
@@ -2,7 +2,6 @@ import React from "react";
 import AssessmentsList from "../../components/assessments-list";
 
 async function getAssessments() {
-
   const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/assessments`)
   if (!response.ok) {
     return new Error("failed to load assessments")
